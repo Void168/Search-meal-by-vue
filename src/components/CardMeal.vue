d134db3308e335832195b3382402686b75ec8fb1
LPHung <phuhung16820@gmail.com>
2023-09-26 18:14:00 +0700

    miss "view" word in view button

--- src/components/CardMeal.vue
+++ src/components/CardMeal.vue
@@ -16,7 +16,7 @@
 
       <div class="mb-4 flex items-center justify-between">
         <YoutubeButton :href="strYoutube">Youtube</YoutubeButton>
-        <ViewButton :href="strYoutube"></ViewButton>
+        <ViewButton :href="strYoutube">View</ViewButton>
       </div>
     </div>
   </div>
