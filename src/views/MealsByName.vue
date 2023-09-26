44e2a03f5830e18b9235c86c414517424b71a6e3
LPHung <phuhung16820@gmail.com>
2023-09-26 15:52:38 +0700

    styled meal's card

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -7,7 +7,43 @@
       placeholder="Search for Meals"
       @change="searchMeals"
     />
-    <pre>{{ meals }}</pre>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+      <div
+        v-for="meal of meals"
+        :key="meal.idMeal"
+        class="bg-white shadow-lg rounded-xl"
+      >
+        <router-link to="/">
+          <img
+            :src="meal.strMealThumb"
+            :alt="strMeal"
+            class="rounded-t-xl w-full h-64 object-cover"
+          />
+        </router-link>
+        <div class="p-3">
+          <p class="font-semibold text-2xl">{{ meal.strMeal }}</p>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            quis magnam voluptate quo.
+          </p>
+
+          <div class="mb-4 flex justify-between">
+            <a
+              href="meal.strYoutube"
+              target="_blank"
+              class="px-4 py-2 border-2 ring-2 ring-offset-2 ring-white border-white bg-rose-500 text-white rounded-lg font-semibold hover:border-rose-400 hover:bg-white hover:ring-rose-400 hover:text-rose-500 transition-colors duration-250"
+              >Youtube</a
+            >
+            <router-link
+              to="/"
+              class="px-4 py-2 border-2 ring-2 ring-offset-2 ring-white border-white bg-indigo-500 text-white rounded-lg font-semibold hover:border-indigo-400 hover:bg-white hover:ring-indigo-400 hover:text-indigo-500 transition-colors duration-250"
+            >
+              View
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
